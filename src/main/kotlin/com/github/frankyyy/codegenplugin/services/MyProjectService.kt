03bf43e99e017c56453e1199405ea4afbae68a6a
GitHub Action <action@github.com>
2022-07-12 14:27:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/frankyyy/codegenplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/frankyyy/codegenplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.frankyyy.codegenplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.frankyyy.codegenplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
